48746c7064f09fdfce3836951be0a0b784f7d2c8
Al Ktifan, Refaat <Refaat.AlKtifan@jochen-schweizer.de>
2021-01-13 20:56:11 +0100

    add admin editUser blade

---
+++ Application/resources/views/admin/editUser.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+@section('title')
+Edit User
+@stop
+
+@section('content')
+<div class="pageHeader">
+<div class="container">
+    <div class="row">
+    <h3>Edit User</h3>
+    </div>
+   </div>
+</div>
+
+
+<div class="container">
+    <div class="row">
+       <form class="form-horizontal" method="POST" action="{{ url('manage/user/edit/'.$user->id) }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ $user['name'] }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $user['email'] }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+ 
+ 
+ 
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Update Staff
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+
+   </div>
+   </div>
+
+@endsection
